cde7be39c0d45633d8e3e210d1bd585fd7b2d557
isaias <isaias.gutierrez48@gmail.com>
2025-07-22 22:15:17 -0700

    background image slight adjust

--- bank/src/main/resources/static/JS/homepage.js
+++ bank/src/main/resources/static/JS/homepage.js
@@ -1,54 +1,52 @@
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-  
+  console.log("Login submitted for:", username);
 
-      document.getElementById("loginForm").addEventListener("submit", (e) => {
-          e.preventDefault();
+  fetch("/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      if (!data.success) {
+        const resultElement = document.getElementById("result");
+        resultElement.style.visibility = "visible";
+        resultElement.innerText = data.message;
+      } else if (data.success) {
+        sessionStorage.setItem("CurrentUser", data.username);
+        sessionStorage.setItem("balance", data.balance);
+        sessionStorage.setItem("accountNumber", data.accountNumber);
 
-          const username = document.getElementById("username").value;
-          const password = document.getElementById("password").value;
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
 
-          console.log("Login submitted for:", username);
-          
-          fetch("/api/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              username: username,
-              password: password
-            }),
-          })
-            .then((res) => {
-              if (!res.ok) {
-                throw new Error("Network response was not ok");
-              }
-              return res.json();
-            })
-            .then((data) => {
-              if (!data.success) {
-                document.getElementById("result").innerText = data.message;
-              }
-              else if (data.success) {
-                sessionStorage.setItem("CurrentUser", data.username);
-                sessionStorage.setItem("balance", data.balance);
-                sessionStorage.setItem("accountNumber", data.accountNumber);
+        window.location.href = "../HTML/Account.html";
+      }
+    })
+    .catch((err) => {
+      const resultElement = document.getElementById("result");
+      resultElement.style.visibility = "visible";
+      resultElement.innerText = "Login failed or server error";
+      resultElement.style.backgroundColor = "#f0f0f063";
 
-                document.getElementById("username").value = "";
-                document.getElementById("password").value = "";
+      console.error(err);
+    });
+});
 
-                window.location.href = "../HTML/Account.html";
-              }
-            })
-            .catch((err) => {
-              document.getElementById("result").innerText =
-                "Login failed or server error.";
-              console.error(err);
-            });
-            
-        });
-
-
-        document.getElementById("Create-Account").addEventListener("click", () => {
-            console.log("clicked");
-            window.location.href = "../HTML/AccountCreation.html";
-        })
+document.getElementById("Create-Account").addEventListener("click", () => {
+  console.log("clicked");
+  window.location.href = "../HTML/AccountCreation.html";
+});
